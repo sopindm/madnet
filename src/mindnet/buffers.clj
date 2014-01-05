eec00fc82a026c81ed703ded58b608b1af25af4f
Dmitry Sopin <sopind@yandex-team.ru>
2014-01-05 13:46:57 +0400

    Added methods for byte arrays reading/writing.

--- src/mindnet/buffers.clj
+++ src/mindnet/buffers.clj
@@ -1,34 +1,91 @@
 (ns mindnet.buffers
   (:import [java.nio ByteBuffer]))
 
+;;
+;; Buffers definition
+;;
+
 (def ^:dynamic *buffer-capacity* 1024)
 
 (defn buffer []
   (let [b (ByteBuffer/allocate *buffer-capacity*)]
     {:writer b :reader (.limit (.duplicate b) 0)}))
 
-(defn write-byte! [{:keys [reader writer] :as buffer} b]
-  (when (= (.position writer) (.capacity writer))
-    (.position writer 0)
-    (.limit writer (.position reader)))
-  (.put writer b)
-  (if (and (<= (.position writer) (.position reader))
-           (= (.limit writer) (.position reader)))
-    (.limit reader (.capacity writer))
-    (.limit reader (.position writer)))
+(defn capacity [b]
+  (.capacity (:writer b)))
+
+(defn- before? [^ByteBuffer b1 ^ByteBuffer b2]
+  (< (.position b1) (.position b2)))
+
+(defn- size- [b]
+  (- (.limit b) (.position b)))
+
+(defn size [{:keys [reader writer]}]
+  (if (before? reader writer)
+    (size- reader)
+    (+ (size- reader) (.position writer))))
+
+(defn free-space [{:keys [reader writer]}]
+  (if (before? writer reader)
+    (size- writer)
+    (+ (size- writer) (.position reader))))
+;;
+;; Generic reading/writing
+;;
+
+(defn- limit- [^ByteBuffer b1 ^ByteBuffer b2]
+  (when (before? b1 b2)
+    (.limit b1 (.position b2)))
+  b1)
+
+(defn- reset- [^ByteBuffer b1 ^ByteBuffer b2]
+  (when (= (.position b1) (.capacity b1))
+    (.position b1 0)
+    (.limit b1 (.position b2)))
+  b1)
+
+(defn- sync- [b1 b2]
+  (limit- b2 b1)
+  (reset- b1 b2))
+
+(defn write-byte! [{:keys [reader ^ByteBuffer writer] :as buffer} b]
+  (.put writer ^byte b)
+  (sync- writer reader)
   buffer)
 
-(defn peek-byte [{reader :reader}]
+(defn- write-bytes-! [{:keys [reader writer]} bytes offset size]
+  (.put writer bytes offset size)
+  (sync- writer reader))
+
+(defn write-bytes! [{:keys [reader writer]:as buffer} bytes]
+  (let [size1 (min (count bytes) (size- writer))
+        size2 (- (count bytes) size1)]
+    (write-bytes-! buffer bytes 0 size1)
+    (when (pos? size2) (write-bytes-! buffer bytes size1 size2)))
+  buffer)
+
+(defn peek-byte [{^ByteBuffer reader :reader}]
   (.mark reader)
   (let [val (.get reader)]
     (.reset reader)
     val))
 
-(defn read-byte! [{:keys [reader writer]}]
+(defn read-byte! [{:keys [^ByteBuffer reader writer]}]
   (let [value (.get reader)]
-    (when (< (.position writer) (.position reader))
-      (.limit writer (.position reader)))
-    (when (= (.position reader) (.capacity reader))
-      (.position reader 0)
-      (.limit reader (.position writer)))
+    (sync- reader writer)
     value))
+
+(defn- read-bytes-! [{:keys [reader writer]} buffer offset size]
+  (.get reader buffer offset size)
+  (sync- reader writer))
+
+(defn read-bytes! [{:keys [reader writer] :as buffer} size]
+  (let [value (byte-array size)
+        size1 (min (size- reader) size)
+        size2 (- size size1)]
+    (read-bytes-! buffer value 0 size1)
+    (when (pos? size2) (read-bytes-! buffer value size1 size2))
+    value))
+
+
+        
--- test/mindnet/buffers_test.clj
+++ test/mindnet/buffers_test.clj
@@ -1,6 +1,7 @@
 (ns mindnet.buffers-test
   (:require [khazad-dum.core :refer :all]
-            [mindnet.buffers :refer :all]))
+            [mindnet.buffers :refer :all])
+  (:import [java.nio BufferUnderflowException BufferOverflowException]))
 
 (deftest writing-and-reading-bytes
   (let [b (buffer)]
@@ -11,8 +12,8 @@
 
 (deftest reading-from-empty-buffer
   (let [b (buffer)]
-    (?throws (peek-byte b) java.nio.BufferUnderflowException)
-    (?throws (read-byte! b) java.nio.BufferUnderflowException)))
+    (?throws (peek-byte b) BufferUnderflowException)
+    (?throws (read-byte! b) BufferUnderflowException)))
 
 (deftest overflowing-buffer
   (let [b (buffer)]
@@ -21,7 +22,7 @@
   (let [b (buffer)]
     (?throws (dotimes [_ (inc *buffer-capacity*)]
                (write-byte! b (byte 123)))
-             java.nio.BufferOverflowException)))
+             BufferOverflowException)))
 
 (deftest write-read-sequence
   (let [b (buffer)]
@@ -29,17 +30,65 @@
       (write-byte! b (byte 123))
       (when (zero? (mod i 2))
         (?= (read-byte! b) 123)))
-    (?throws (write-byte! b (byte 123)) java.nio.BufferOverflowException)
+    (?throws (write-byte! b (byte 123)) BufferOverflowException)
     (dotimes [i *buffer-capacity*]
-      (?= (read-byte! b) 123))))
+      (?= (read-byte! b) 123))
+    (?throws (read-byte! b) BufferUnderflowException)))
 
-;writing and reading different java types
+(deftest buffer-size-capacity-and-free-space
+  (let [b (buffer)]
+    (?= (capacity b) *buffer-capacity*)
+    (?= (size b) 0)
+    (?= (free-space b) *buffer-capacity*)
+    (dotimes [i *buffer-capacity*]
+      (write-byte! b (byte 123)))
+    (?= (capacity b) *buffer-capacity*)
+    (?= (size b) *buffer-capacity*)
+    (?= (free-space b) 0)
+    (dotimes [i (quot *buffer-capacity* 2)]
+      (read-byte! b))
+    (?= (capacity b) *buffer-capacity*)
+    (?= (size b) (- *buffer-capacity* (quot *buffer-capacity* 2)))
+    (?= (free-space b) (quot *buffer-capacity* 2))))
+
+(deftest buffer-ring-size-and-capacity
+  (let [b (buffer)]
+    (dotimes [i (dec *buffer-capacity*)]
+      (write-byte! b (byte 123)))
+    (dotimes [i (quot *buffer-capacity* 2)]
+      (read-byte! b))
+    (?= (free-space b) (inc (quot *buffer-capacity* 2)))
+    (dotimes [i (quot *buffer-capacity* 2)]
+      (write-byte! b (byte 123)))
+    (?= (size b) (dec *buffer-capacity*))))
+
+(deftest writing-and-reading-byte-arrays
+  (let [b (buffer)
+        byte-seq (fn [size] (map byte (take size (cycle (range -128 128)))))]
+    (write-bytes! b (byte-array (byte-seq *buffer-capacity*)))
+    (?= (seq (read-bytes! b *buffer-capacity*))
+        (take *buffer-capacity* (cycle (range -128 128))))
+    (?= (free-space b) (capacity b))
+    (write-bytes! b (byte-array (byte-seq (quot *buffer-capacity* 2))))
+    (?= (seq (read-bytes! b (quot *buffer-capacity* 2)))
+        (byte-seq (quot *buffer-capacity* 2)))
+    (write-bytes! b (byte-array (byte-seq *buffer-capacity*)))
+    (?= (seq (read-bytes! b *buffer-capacity*)) (byte-seq *buffer-capacity*))))
+
+;switch from *buffer-capacity* to (capacity b)
+;read bytes in existing array
+;writing bytes from byte seq's
+
+;writing and reading too much
+
+;reading and writing slices (chunks)
+
+;conditional writes and read
+
 ;writing and reading byte arrays
-;writing and reading strings
-;writing and reading clojure objects (+generic)
+;writing and reading strings (+encoding, default encoding)
 
-;concatenating buffers
-;getting subbuffers
+;splitting and concatenating
 ;seq + string wrapper
 
 ;benchmarks???
