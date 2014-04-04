c0ec5aa688a70784c1761f571324630ec7de7435
Dmitry Sopin <sopind@yandex-team.ru>
2014-04-04 23:15:27 +0400

    Moved trigger signal to separate file

--- scala/madnet/event/Signal.scala
+++ scala/madnet/event/Signal.scala
@@ -1,7 +1,5 @@
 package madnet.event;
 
-import java.util.concurrent.TimeUnit
-
 trait ISignal extends IEvent  {
   override def close() { _attachment = null; super.close() }
 
@@ -28,7 +26,7 @@ trait ISignalSetLike[T <: ISignal] extends ISignalSet[T] {
   override def signals = _signals.snapshot
 
   override def +=(signal: T) = {
-    requireOpen
+    requireOpen()
     _signals += signal
     this
   }
@@ -40,70 +38,11 @@ trait ISignalSetLike[T <: ISignal] extends ISignalSet[T] {
   private[this] var _isOpen = true
   def isOpen = _isOpen
 
-  protected def requireOpen {
+  protected def requireOpen() {
     if(!isOpen) throw new java.nio.channels.ClosedSelectorException
   }
 
   override def close { signals.foreach(this -= _); _isOpen = false }
 
   override val selections = new Set[T]
-}
-
-class TriggerSignal extends ISignal {
-  private var _provider: TriggerSet = null
-  def provider = _provider
-
-  override def close() {
-    if(provider != null) provider -= this
-    super.close()
-  }
-
-  private[event] def register(provider: TriggerSet) {
-    require(_provider == null)
-    _provider = provider
-  }
-
-  override var persistent = false
-  override def emit(obj: Any) {
-    super.emit(obj)
-    if(persistent) start()
-  }
-
-  private[event] def cancel { _provider = null }
-
-  override def start { provider.trigger(this) }
-}
-
-
-class TriggerSet extends ISignalSetLike[TriggerSignal] {
-  private[this] val triggered = new java.util.concurrent.LinkedBlockingQueue[TriggerSignal]()
-  private[event] def trigger(s: TriggerSignal) { triggered.add(s) }
-
-  private def _push(s: TriggerSignal) { if(s != null) selections += s }
-
-  override def select {
-    requireOpen
-    if(!isEmpty) _push(triggered.take())
-    selectNow
-  }
-
-  override def selectIn(millis: Int) {
-    requireOpen
-    if(!isEmpty) _push(triggered.poll(millis, TimeUnit.MILLISECONDS));
-    selectNow
-  }
-
-  override def selectNow {
-    requireOpen
-    while(triggered.size() > 0)
-      _push(triggered.poll())
- }
-
-  override def +=(signal: TriggerSignal) = {
-    signal.register(this); super.+=(signal)
-  }
-
-  override def -=(signal: TriggerSignal) = {
-    signal.cancel; super.-=(signal)
-  }
 }
